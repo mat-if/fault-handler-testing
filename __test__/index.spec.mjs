5ab26a8fb63c32aa2dce410435a8f7465efa013b
Mat <mat@ironfish.network>
2023-02-28 14:13:04 -0700

    0.0.1

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { initFaultHandler, sum } from '../index.js'
+import { initFaultHandler, triggerSignal } from '../index.js'
 
 test('inits the handler', (t) => {
   initFaultHandler()
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,27 @@
 #[macro_use]
 extern crate napi_derive;
 
+extern "C" {
+  // TODO: This isnt present in linux libc
+  fn backtrace_symbols_fd(buffer: *const *mut libc::c_void, size: libc::c_int, fd: libc::c_int);
+}
+
 /// # Safety
 /// This is unsafe, it calls libc functions
+#[cfg(all(unix, target_env = "musl"))]
+unsafe fn display_trace() {
+  libc::exit(libc::EXIT_FAILURE);
+}
+
+/// # Safety
+/// This is unsafe, it calls libc functions
+#[cfg(all(unix, not(target_env = "musl")))]
 unsafe fn display_trace() {
   // Borrowed from https://github.com/rust-lang/rust/blob/90677edcba063ee83316ef109e0fe54116015575/compiler/rustc_driver_impl/src/lib.rs#L1297-L1308
   const MAX_FRAMES: usize = 256;
   static mut STACK_TRACE: [*mut libc::c_void; MAX_FRAMES] = [std::ptr::null_mut(); MAX_FRAMES];
   let depth = libc::backtrace(STACK_TRACE.as_mut_ptr(), MAX_FRAMES as i32);
-  libc::backtrace_symbols_fd(STACK_TRACE.as_ptr(), depth, 2);
+  backtrace_symbols_fd(STACK_TRACE.as_ptr(), depth, 2);
   libc::exit(libc::EXIT_FAILURE);
 }
 
